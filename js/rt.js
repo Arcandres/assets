95ab59c48dd693213b333aae8212cda7a7587f49
Arcandres <Arcandres@hotmail.com>
2018-05-25 01:11:10 -0500

    Reading time plugin

---
+++ js/rt.js
@@ -0,0 +1,15 @@
+!function () {
+  var t   = document.getElementById('time'),
+      p = `<data:post.body/>`,
+      wpm = 200;
+
+  p = pb.split(' ');
+  p = Math.round(p.length / wpm);
+
+  if (p < 0)
+    p.textContent = 'Menos de 1 min';
+  else if (p > 1)
+    t.textContent = ' ' + p + ' min'
+  else
+    t.textContent = ' ' + p + ' mins';
+}();
